ad1c680a7851b6c5a47668e4552e64f39887de27
Myeong <caps813@gmail.com>
2023-10-07 22:22:55 +0900

    addt to word tokenization

---
+++ nlp_preprocessing_4.py
@@ -0,0 +1,8 @@
+
+# word tokenization
+
+from nltk import word_tokenize
+sentence = "Natural language processing is one of the more challenging fields in AI."
+words = word_tokenize(sentence)
+print(words)
+
